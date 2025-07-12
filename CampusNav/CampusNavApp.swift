09346ab8615c3af397432a64cb2ae04b6c464d40
JonahBlackmon <JonahKBlackmon@gmail.com>
2025-07-12 13:50:59 -0500

    Initial Commit

---
+++ CampusNav/CampusNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusNavApp.swift
+//  CampusNav
+//
+//  Created by Jonah Blackmon on 7/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CampusNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CampusNav/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CampusNav
+//
+//  Created by Jonah Blackmon on 7/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
