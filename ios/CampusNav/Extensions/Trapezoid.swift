eda111dcf4fbbcf5d5bcdff61de681e4f2c2fe9a
JonahBlackmon <JonahKBlackmon@gmail.com>
2025-07-28 17:00:22 -0500

    Created Trapzeoid file and moved struct

---
+++ ios/CampusNav/Extensions/Trapezoid.swift
@@ -0,0 +1,31 @@
+//
+//  Trapezoid.swift
+//  CampusNav
+//
+//  Created by Jonah Blackmon on 7/28/25.
+//
+import SwiftUI
+
+struct Trapezoid: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let width = rect.width
+        let height = rect.height
+
+        let topWidthRatio: CGFloat = 0.85
+        let bottomWidthRatio: CGFloat = 1.2
+
+        let topWidth = width * topWidthRatio
+        let bottomWidth = width * bottomWidthRatio
+        let xOffset = (width - topWidth) / 2
+        let bottomOffset = (width - bottomWidth) / 2
+
+        path.move(to: CGPoint(x: xOffset, y: 0))                          // Top left
+        path.addLine(to: CGPoint(x: width - xOffset, y: 0))              // Top right
+        path.addLine(to: CGPoint(x: width - bottomOffset, y: height))    // Bottom right
+        path.addLine(to: CGPoint(x: bottomOffset, y: height))            // Bottom left
+        path.closeSubpath()
+
+        return path
+    }
+}
--- ios/CampusNav/Views/Map/LocationPuck.swift
+++ ios/CampusNav/Views/Map/LocationPuck.swift
@@ -52,32 +52,3 @@ struct LocationPuck: View {
         }
     }
 }
-
-struct Trapezoid: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        let width = rect.width
-        let height = rect.height
-
-        // Adjustable trapezoid proportions
-        let topWidthRatio: CGFloat = 0.85  // 50% of total width
-        let bottomWidthRatio: CGFloat = 1.2 // full width
-
-        let topWidth = width * topWidthRatio
-        let bottomWidth = width * bottomWidthRatio
-        let xOffset = (width - topWidth) / 2
-        let bottomOffset = (width - bottomWidth) / 2
-
-        path.move(to: CGPoint(x: xOffset, y: 0))                          // Top left
-        path.addLine(to: CGPoint(x: width - xOffset, y: 0))              // Top right
-        path.addLine(to: CGPoint(x: width - bottomOffset, y: height))    // Bottom right
-        path.addLine(to: CGPoint(x: bottomOffset, y: height))            // Bottom left
-        path.closeSubpath()
-
-        return path
-    }
-}
-
-//#Preview {
-//    LocationPuck()
-//}
